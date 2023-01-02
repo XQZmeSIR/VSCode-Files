b8be4d334d6c1fd5da5826e80a4a3dea9c45f81d
XQZmeSIR <rys2000lan7733@mail.ru>
2023-01-02 19:09:33 +0300

    исправления хз какие

---
+++ Indi_Course_Egoroff.py
@@ -0,0 +1,22 @@
+a = 30
+b = 50
+
+# b = b + 1
+b += 1
+print('b =', b)
+
+# a = a + b
+a += b
+print('a =', a)
+
+# a = a + 100
+a += 100
+print('a =', a)
+
+# b = b - a
+b -= a
+print('b =', b)
+
+# a = a - 80
+a -= 80
+print('a =', a)
---
+++ List sorting.py
@@ -0,0 +1,79 @@
+# cars = ['bmw', 'audi', 'toyota', 'subary']
+# cars.sort()  # Сортировка(постоянная, без возврата) списка в АЛФАВИТНОМ порядк.
+# print(cars)
+
+# cars = ['bmw', 'audi', 'toyota', 'subary']
+# # Сортировка(постоянная, без возврата) списка в ОБРАТНОМ АЛФАВИТНОМ порядке
+# cars.sort(reverse=True)
+# print(cars)
+
+# cars = ['bmw', 'audi', 'toyota', 'subary']
+
+# print("Here is the original list:")
+# print(cars)
+# # сохранить исходный порядок элементов списка, но временно представить их в отсортированном АЛФАВИТНОМ порядке
+# print("\nHere is sorted list:")
+# print(sorted(cars))
+
+
+# print("\nHere is the original list again:")
+# print(cars)
+
+
+#  reverse() Сортирует список в просто обратном порядке
+# НЕ осуществ постоянное измененике списка
+#  снова применив reverse() вернемся к обратному списку
+
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+# print(cars)
+# cars.reverse()
+# print(cars)
+
+
+# #  быстро определить длину списка с помощью функции len().
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+# print(len(cars))
+
+
+
+# EXERCISE 3.8
+
+countries = ['italy', 'america', 'germany', 'island']
+print(countries)
+
+print(sorted(countries))
+
+print(countries)
+
+countries.reverse()
+print(countries)
+
+countries.reverse()
+print(countries)
+
+
+countries.sort()
+print(len(countries))
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles[-1])
+
+print('Hello, world') 
+
+
+abc = ['heu', 'hhdvwv', 'hwvuw']
+del abc[1]
+print(abc)
+
+
+
+
+
+
+
+
+
+
+
+
+
